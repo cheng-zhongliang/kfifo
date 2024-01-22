cb3128b824258d39e8fac00c5367d47ec57ec6f0
pilot <chengzhongliang@wsdashi.com>
2024-01-22 17:59:59 +0800

    test thread safe

--- test_kfifo_thread_safe.c
+++ test_kfifo_thread_safe.c
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 cheng-zhongliang. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 #include <assert.h>
 #include <pthread.h>
 #include "kfifo.h"
