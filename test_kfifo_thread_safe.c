096852a9677c4d8ab5de5db83215fe78513ff2f8
pilot <chengzhongliang@wsdashi.com>
2024-01-22 17:21:11 +0800

    test thread safe

---
+++ test_kfifo_thread_safe.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <pthread.h>
+#include <stdio.h>
+#include "kfifo.h"
+
+void* producer_thread(void* arg) {
+    _KFIFO(kfifo, int)* kfifo = arg;
+    int n = 0;
+    do {
+        KFIFO_ENQUEUE(kfifo, n);
+        n++;
+    } while(!KFIFO_FULL(kfifo) && n != 1000);
+}
+
+void* consumer_thread(void* arg) {
+    _KFIFO(kfifo, int)* kfifo = arg;
+    int n = 0;
+    do {
+        assert(n == KFIFO_DEQUEUE(kfifo));
+        n++;
+    } while(!KFIFO_EMPTY(kfifo) && n != 1000);
+}
+
+int main(void) {
+    _KFIFO(kfifo, int) kfifo;
+    KFIFO_INIT(&kfifo, 16);
+    pthread_t producer, consumer;
+    pthread_create(&producer, NULL, producer_thread, &kfifo);
+    pthread_create(&consumer, NULL, consumer_thread, &kfifo);
+    pthread_join(producer, NULL);
+    pthread_join(consumer, NULL);
+    return 0;
+}
